6eedd8fcbe5ef2d77dfd8bc279364f800e2a1f38
nancy-heeney <niheeney@mavs.coloradomesa.edu>
2024-02-13 15:11:09 -0700

    oww

--- Notes/today.cpp
+++ Notes/today.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    //Display Welcome to C++ <<endl;
-    cout <<"Welcome to C++" << endl;
-
+    cout<< "Enter x1 and y1: ";
+    float x1, y1;
+    cin>> x1 >> y1;
+    cout<< " Enter x2 and y2: ";
+    float x2, y2;
+    cin>> x2 >> y2;
+    float distance;
+    distance = sqrt( pow ( ( x2 - x1), 2 ) + pow ( ( y2 - y1), 2 ) );
+    cout<< "The distance between the two points is " << distance;
     return 0;
 }
