7cb0875e0505fa1999d9f67525013656c7176afb
nancy-heeney <niheeney@mavs.coloradomesa.edu>
2024-05-08 00:48:36 -0600

    done

---
+++ Final_Project/words.cpp
@@ -0,0 +1,6 @@
+string words[NUM_WORDS] = {
+    "apple", "banana", "cherry", "date", "elderberry",
+    "fig", "grape", "honeydew", "ice cream", "jackfruit",
+    "kiwi", "lemon", "mango", "nectarine", "orange",
+    "pineapple", "quince", "raspberry", "strawberry", "tangerine"
+};
