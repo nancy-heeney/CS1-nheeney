c3bdc15accf72336c79d3bcbc919cdf077a931aa
nancy-heeney <niheeney@mavs.coloradomesa.edu>
2024-05-09 20:58:37 -0600

    done

--- Assignments/Labs/FileIO/main.cpp
+++ Assignments/Labs/FileIO/main.cpp
@@ -51,35 +51,79 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-void readData(vector<int> & numbers, const string inputFileName) {
-    // FIXME1: Open inputFileName for reading data
+void readData(vector<int> & numbers, const string inputFileName) 
+{
+    // FIXED    
     // read the data until eof marker and store each num into numbers vector
+    std::ifstream file(inputFileName);
+    if (file.is_open()) 
+    {
+        // Read the data until eof marker and store each num into numbers vector
+        while (file >> num) 
+        {
+            numbers.push_back(num);
+        }
+        file.close();
+    } 
+    else 
+    {
+        std::cout << "Unable to open file" << std::endl;
+    }
     int num;
 }
 
-void writeData(const vector<int> & numbers) {
-    // FIXME2
+void writeData(const vector<int> & numbers) 
+{
+    // FIXED
     /*
     Algorithm steps:
     1. Prompt user to enter output file name
     2. Store and use the file name to open the file in write mode
     3. Write output as shown in output.txt file with proper formatting using iomanip
     */
+   std::string outputFileName;
+    std::cout << "Enter output file name: ";
+    std::cin >> outputFileName;
+
+    // Open the file in write mode
+    std::ofstream file(outputFileName);
+
+    // Write output as shown in output.txt file with proper formatting using iomanip
+    for (size_t i = 0; i < numbers.size(); i++) 
+    {
+        file << std::setw(5) << i + 1 << ": " << std::setw(5) << numbers[i] << std::endl;
+    }
+
+    file.close();
 }
 
-int findMax(const vector<int> & nums) {
+int findMax(const vector<int> & nums) 
+{
     int max = nums[0];
     for(int n: nums)
         max = (n>max) ? n : max;
     return max;
 }
 
-int findMin(const vector<int> & nums) {
-    // FIXME3 - implement function to find and return min value in nums vector
+int findMin(const vector<int> & nums) 
+{
+    // FIXED - implement function to find and return min value in nums vector
+    auto min_it = std::min_element(nums.begin(), nums.end());
+
+    // Check if nums is non-empty and return min value
+    if (min_it != nums.end()) 
+    {
+        return *min_it;
+    } 
+    else 
+    {
+        return 0;
+    }
     return 0;
 } 
 
-float findMean(const vector<int> & nums) {
+float findMean(const vector<int> & nums) 
+{
     // same as average
     long long int sum = 0;
     for(int n: nums)
@@ -87,13 +131,20 @@ float findMean(const vector<int> & nums) {
     return sum/float(nums.size());
 }
 
-int findRange(const vector<int> & nums) {
-    // FIXME6 - implement function that finds and returns the range value
+int findRange(const vector<int> & nums) 
+{
+    // FIXED - implement function that finds and returns the range value
     // range = max - min
+    int min = *std::min_element(nums.begin(), nums.end());
+    int max = *std::max_element(nums.begin(), nums.end());
+
+    return max - min;
+    
     return 0;
 }
 
-float findMedian(vector<int> nums) {
+float findMedian(vector<int> nums) 
+{
     sort(nums.begin(), nums.end());
     float median;
     if (nums.size() % 2 == 0) {  // even set
@@ -104,7 +155,8 @@ float findMedian(vector<int> nums) {
     return median;
 }
 
-void test() {
+void test() 
+{
     vector<int> numbers = {100, 10, 5, 0, -99, 10, 99};
     //cout << findMean(numbers) << " " << findMean(numbers)-17.857142 << endl;
     assert(fabs(findMean(numbers)-17.857142) <= EPSILON );
@@ -114,7 +166,9 @@ void test() {
     assert(fabs(findMean(numbers1) -1.6667) <= EPSILON  );
     assert(findMax(numbers1) == 10);
     assert(findMedian(numbers1) == 5);
-    // FIXME7: Write at least two test cases for other functions
+    // FIXED: Write at least two test cases for other functions
+    assert(findMin(numbers) == -99);
+    assert(findRange(numbers) == 198);
 
     cerr << "all test cases passed!\n";
 }
